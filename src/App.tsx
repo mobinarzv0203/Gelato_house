453e21811c293e880e17cf6fcddc934ffcf3c1f7
mobinarzv0203 <mobina.razavi0203@gmail.com>
2026-01-06 18:15:26 +0330

    context changed

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-green-100">
-      Hello World!!
+    <div className="bg-red-100">
+      سلام بر شما!!
     </div>
   )
 }
