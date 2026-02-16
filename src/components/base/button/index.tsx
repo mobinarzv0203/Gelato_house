9ac1f9291b8430eee53b86a3907a8f9350d68271
mobinarzv0203 <mobina.razavi0203@gmail.com>
2026-02-16 19:52:28 +0330

    nice changes

--- src/components/base/button/index.tsx
+++ src/components/base/button/index.tsx
@@ -5,20 +5,31 @@ import { clx } from "../../../utilities/clx";
 type ButtonProps = {
   variant?: "contained" | "outlined" | "text";
   color?: "primary" | "secondary";
+  size?: "small" | "medium" | "large";
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const colors = { primary: "bg-primary-500", secondary: "bg-secondary-500" };
+const colors = { 
+  primary: "bg-primary-500 text-white", 
+  secondary: "bg-secondary-500 text-white" 
+};
+
+const sizes = {
+  small: "px-2 py-1 text-sm", 
+  medium: "px-4 py-2 text-base",
+  large: "px-6 py-3 text-lg"
+}
 export const Button: React.FC<ButtonProps> = ({
   variant = "contained",
   children,
   color = "primary",
+  size = "medium",
   ...props
 }): JSX.Element => {
   switch (variant) {
     case "contained":
       return (
         <button
-          className={clx("bg-primary-600 p-2 rounded-3xl", colors[color])}
+          className={clx("p-2 rounded-3xl", colors[color], sizes[size])}
           {...props}
         >
           {children}
